ab3b9d984d3946368ab0b6a30323aecf91e29063
Gabriel Bravo <116980060+vestelion@users.noreply.github.com>
2023-08-07 22:40:30 +0200

    Update decode.py

--- src/decode.py
+++ src/decode.py
@@ -1,23 +1,23 @@
 import base64
 
-# Leer el video codificado
+# Read the encoded video
 encoded_video_path = "output/video_c.mp4"
 with open(encoded_video_path, "rb") as encoded_video_file:
     encoded_video_data = encoded_video_file.read()
 
-# Encontrar el separador en el video
+# Find the separator in the video
 separator = b"VIDEOJSONSEPARATOR"
 separator_index = encoded_video_data.rfind(separator)
 if separator_index == -1:
     raise ValueError("Separador no encontrado en el video")
 
-# Extraer los datos codificados del JSON del video
+# Extract the encoded JSON data from the video
 video_data = encoded_video_data[:separator_index]
 encoded_json = encoded_video_data[separator_index + len(separator):]
 
-# Decodificar la cadena codificada en base64
+# Decode the base64-encoded string
 decoded_json = base64.b64decode(encoded_json).decode()
 
-# Mostrar el JSON decodificado en la consola
+# Display the decoded JSON in the console
 print("JSON decodificado:")
 print(decoded_json)
